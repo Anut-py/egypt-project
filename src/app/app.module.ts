325b42338945859fd76e92e57a37085b12e885d3
Anand Swaroop <pritirekha@gmail.com>
2021-06-02 09:03:00 -0400

    Added old and new pyramid models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {ModelViewerComponent} from './model-viewer/model-viewer.component';
+import {MatButtonModule} from '@angular/material/button';
+import {HomeComponent} from './home/home.component';
+import {HowToComponent} from './how-to/how-to.component';
+import {StructuresComponent} from './structures/structures.component';
+import {GalleryComponent} from './gallery/gallery.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'how-to', component: HowToComponent},
+  {path: 'structures', component: StructuresComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelViewerComponent,
+    HomeComponent,
+    HowToComponent,
+    StructuresComponent,
+    GalleryComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://anut-py.github.io/egypt-project/'
 };
